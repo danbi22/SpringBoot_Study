b5a615d2c175ca740598b548c994e8fab633d6fa
yudahan <103639913+danbi22@users.noreply.github.com>
2023-06-27 09:39:36 +0900

    spring boot

---
+++ spring3/src/main/java/com/itwill/spring3/repository/post/PostRepository.java
@@ -0,0 +1,12 @@
+package com.itwill.spring3.repository.post;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long>{
+
+	// id 내림차순 정렬:
+	// select * from posts order by id desc
+	List<Post> findByOrderByIdDesc();
+}
